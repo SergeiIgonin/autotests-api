9fcccbfe0bb0597dfc985f980e859d4253b2435c
SergeiIgonin <igonin.sergei3@gmail.ru>
2026-08-08 17:25:43 +0300

    Добавление pydentic классов для всех клиентов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,23 +1,27 @@
-from typing import TypedDict
-
 from httpx import Client
+from pydantic import BaseModel
 
-from clients.authentication.authentication_client import get_authentication_client, LoginRequestDict
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginRequestSchema
 
 
-class AuthenticationUserDict(TypedDict):
-    """Описание структуры данных пользователя для авторизации."""
+# Добавили суффикс Schema вместо Dict
+class AuthenticationUserSchema(BaseModel):  # Наследуем от BaseModel вместо TypedDict
     email: str
     password: str
 
 
-def get_private_http_client(user: AuthenticationUserDict) -> Client:
-    """Функция создаёт экземпляр httpx.Client с аутентификацией пользователя.
-
-    :param user: Объект AuthenticationUserSchema с email и паролем пользователя.
-    :return: Готовый к использованию объект httpx.Client с установленным заголовком Authorization.
-    """
+def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     authentication_client = get_authentication_client()
-    login_request = LoginRequestDict(email=user['email'], password=user['password'])
+
+    # Используем модель LoginRequestSchema
+    # Значения теперь извлекаем не по ключу, а через атрибуты
+    login_request = LoginRequestSchema(email=user.email, password=user.password)
     login_response = authentication_client.login(login_request)
-    return Client(timeout=100, base_url="http://localhost:8000", headers={"Authorization": f"Bearer {login_response['token']['accessToken']}"})
+
+    return Client(
+        timeout=100,
+        base_url="http://localhost:8000",
+        # Значения теперь извлекаем не по ключу, а через атрибуты
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
+    )
